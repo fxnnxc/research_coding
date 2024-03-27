852d487241659394800e297539e8c5a21790b420
fxnnxc <bumjin@kaist.ac.kr>
2024-03-27 20:48:53 +0900

    udpate the hook

--- shells/kakao.sh
+++
@@ -1,9 +0,0 @@
-
-data_name='pokemon' #flickr30k 
-model_name="openai/clip-vit-large-patch14"  # openai/clip-vit-base-patch32
-save_dir="outputs/"$data_name"/"$model_name
-python scripts/kakao_captioning.py \
-    --save_dir $save_dir \
-    --data_name $data_name \
-    --model_name $model_name
-    
